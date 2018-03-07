40df2fe021056fe8aeffb6c73bb49ef02fd45048
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-07 22:29:28 +1100

    Add PropTypes for withRouter

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,5 +1,6 @@
 import { Component } from 'react'
 import { withRouter } from 'react-router'
+import PropTypes from 'prop-types'
 
 class ScrollToTop extends Component {
   componentDidUpdate(prevProps) {
@@ -11,6 +12,12 @@ class ScrollToTop extends Component {
   render() {
     return this.props.children
   }
+}
+
+ScrollToTop.propTypes = {
+  match: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
 }
 
 export default withRouter(ScrollToTop)
